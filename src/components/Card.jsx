b05fe637655b45fa4f26d31a21a55d69d8e03fb2
bigacz <sbigacz@gmail.com>
2025-02-03 22:55:25 +0100

    add Card documentation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,10 @@
-function Card() {}
+/**
+ *
+ * @param {string} props.text - Text that will be displayed on the card.
+ * @param {string} props.image - Path of image that will be displayed on the card.
+ * @param {function} props.onClick - Function that will be called on click of the card.
+ */
+
+function Card({ text, image, onClick }) {}
 
 export default Card;
