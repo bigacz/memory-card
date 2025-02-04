ca6f03fe9d2aafe31c55b2a51dac084f4cc8bd4f
bigacz <sbigacz@gmail.com>
2025-02-05 00:25:19 +0100

    add EndModal

--- src/components/EndModal.jsx
+++ src/components/EndModal.jsx
@@ -1,3 +1,34 @@
-function EndModal() {}
+/**
+ *
+ * @param {boolean} props.isWin - Displays whether game was won or not.
+ * @param {number} props.score - Displays score.
+ * @param {string} props.difficulty - Displays difficulty.
+ * @param {string} props.genre - Displays genre.
+ * @param {function} props.onNavigateToMenuClick - Function that will be called on click of a main menu button.
+ * @param {function} props.onGameRestartClick - Function that will be called on click of a restart button.
+ * @returns
+ */
+
+function EndModal({
+  isWin,
+  score,
+  difficulty,
+  genre,
+  onNavigateToMenuClick,
+  onGameRestartClick,
+}) {
+  return (
+    <div>
+      <div>
+        <h2>You {isWin ? 'won' : 'lost'}!</h2>
+        <p>Score: {score}</p>
+        <p>Difficulty: {difficulty}</p>
+        <p>Genre: {genre}</p>
+      </div>
+      <button onClick={onNavigateToMenuClick}>Main Menu</button>
+      <button onClick={onGameRestartClick}>Play again</button>
+    </div>
+  );
+}
 
 export default EndModal;
