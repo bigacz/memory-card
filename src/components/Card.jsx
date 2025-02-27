ae1cf10f65b4b3740f91463cb69da1812a97f250
bigacz <sbigacz@gmail.com>
2025-02-27 21:18:13 +0100

    add card hover animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import 'styles/components/Card.css';
 
 /**
@@ -8,14 +9,49 @@ import 'styles/components/Card.css';
  */
 
 function Card({ text, image, onClick }) {
+  const [cursorPercentage, setCursorPosition] = useState(null);
+
+  let xPosition;
+  let yPosition;
+  if (cursorPercentage != null) {
+    xPosition = cursorPercentage.x;
+    yPosition = cursorPercentage.y;
+  }
+
   return (
-    <img
-      onClick={onClick}
-      src={image}
-      alt={text}
-      draggable="false"
-      className="card"
-    />
+    <div
+      onMouseMove={(event) => {
+        const { x, y, width, height } = event.target.getBoundingClientRect();
+        const { clientX, clientY } = event;
+
+        const xPosition = Math.round(clientX - x);
+        const yPosition = Math.round(clientY - y);
+
+        const xMiddleRelative = xPosition - width / 2;
+        const yMiddleRelative = yPosition - height / 2;
+
+        const xPercentage = xMiddleRelative / (width / 2);
+        const yPercentage = (yMiddleRelative / (height / 2)) * -1;
+
+        const newCursorPercentage = { x: xPercentage, y: yPercentage };
+
+        setCursorPosition(newCursorPercentage);
+      }}
+      onMouseLeave={() => {
+        setCursorPosition({ x: 0, y: 0 });
+      }}
+    >
+      <img
+        onClick={onClick}
+        style={{
+          transform: `perspective(1000px) rotate3d(${yPosition}, ${xPosition}, 0, 0.07turn)`,
+        }}
+        src={image}
+        alt={text}
+        draggable="false"
+        className="card"
+      />
+    </div>
   );
 }
 
